392140dcc3781fb939ab980b5ef12d7cd19afcf5
Alendar007 <martin.perard@orange.fr>
2026-08-31 14:03:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-  // Service worker minimal pour valider la PWA
+  // Service worker minimal pour valider et autoriser le mode PWA autonome
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
